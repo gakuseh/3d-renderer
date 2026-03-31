75872d8694ddd6867ca23acd44d1ae2e5de7ab5b
gakuseh <eric@gakuseh.dev>
2026-03-31 11:05:50 -0400

    Get 3D eye positions, apply homography, display

--- main.gd
+++ main.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
 @export var calibration_ui: Control
-@export var left_cameras: Camera3D
-@export var right_cameras: Camera3D
-@export var left_viewport_container: SubViewportContainer
-@export var right_viewport_container: SubViewportContainer
+@export var left_camera: Camera3D
+@export var right_camera: Camera3D
+@export var left_panel: Panel
+@export var right_panel: Panel
 
 var currently_loaded_object: Node
 
@@ -58,8 +58,9 @@ func _process(delta: float) -> void:
 		if waiting_for_request:
 			#print('Must be request code, so has to be a int 64')
 			var request_code: int = conn.get_64()
+			
 
-			#print('Request code is ' + str(request_code))
+			print('RENDERER: Request code is ' + str(request_code))
 
 			match request_code:
 				0:
@@ -69,43 +70,34 @@ func _process(delta: float) -> void:
 					# Hide calibration window
 					calibration_ui.visible = false
 				2: 
+					# Getting pixels_per_lens, which is the only parameter we need from the 
+					# controller
 					var pixels_per_lens = conn.get_float()
-					var index_of_refraction = conn.get_float()
 					print('Received code 2. Pixels per lens is ', pixels_per_lens)
-					print('In addition, the index of refraction is ', index_of_refraction)
-					left_viewport_container.material.set_shader_parameter("pixels_per_lens", pixels_per_lens)
-					right_viewport_container.material.set_shader_parameter("pixels_per_lens", pixels_per_lens)
+					left_panel.material.set_shader_parameter("pixels_per_lens", pixels_per_lens)
+					right_panel.material.set_shader_parameter("pixels_per_lens", pixels_per_lens)
 
-					left_viewport_container.material.set_shader_parameter("index_of_refraction", index_of_refraction)
-					right_viewport_container.material.set_shader_parameter("index_of_refraction", index_of_refraction)
 				3:
 					conn.put_64(get_viewport().get_visible_rect().size.x)
 				4:
-					# New eye angles, so move the camera
-					var left_eye_horizontal_angle: float = conn.get_double()
-					var left_eye_vertical_angle: float = conn.get_double()
-					var right_eye_horizontal_angle: float = conn.get_double()
-					var right_eye_vertical_angle: float = conn.get_double()
+					# New eye positions
+					print('RENDERER: Got new eye positions.')
+					var l_x: float = conn.get_float()
+					var l_y: float = conn.get_float()
+					var l_z: float = conn.get_float()
 					
-					var left_x = cos(deg_to_rad(left_eye_vertical_angle)) * cos(deg_to_rad(left_eye_horizontal_angle))
-					var left_y = -sin(deg_to_rad(left_eye_vertical_angle))
-					var left_z = cos(deg_to_rad(left_eye_vertical_angle)) * sin(deg_to_rad(left_eye_horizontal_angle))
+					var r_x: float = conn.get_float()
+					var r_y: float = conn.get_float()
+					var r_z: float = conn.get_float()
+					
+					left_camera.position = Vector3(l_x, l_y, l_z)
+					right_camera.position = Vector3(r_x, r_y, r_z)
 
-					var right_x = cos(deg_to_rad(right_eye_vertical_angle)) * cos(deg_to_rad(right_eye_horizontal_angle))
-					var right_y = -sin(deg_to_rad(right_eye_vertical_angle))
-					var right_z = cos(deg_to_rad(right_eye_vertical_angle)) * sin(deg_to_rad(right_eye_horizontal_angle))
+					left_camera.look_at(Vector3(0, 0, 0), Vector3.UP)
+					right_camera.look_at(Vector3(0, 0, 0), Vector3.UP)
+					
+					print('Left eye is: ', left_camera.position, 'Right eye is: ', right_camera.position)
 
-					left_cameras.position = Vector3(left_x, left_y, left_z)
-					right_cameras.position = Vector3(right_x, right_y, right_z)
-
-					left_cameras.look_at(Vector3(0, 0, 0), Vector3.UP)
-					right_cameras.look_at(Vector3(0, 0, 0), Vector3.UP)
-
-					left_viewport_container.material.set_shader_parameter("left_eye_angle_degrees", left_eye_horizontal_angle)
-					left_viewport_container.material.set_shader_parameter("right_eye_angle_degrees", right_eye_horizontal_angle)
-
-					right_viewport_container.material.set_shader_parameter("left_eye_angle_degrees", left_eye_horizontal_angle)
-					right_viewport_container.material.set_shader_parameter("right_eye_angle_degrees", right_eye_horizontal_angle)
 				5:
 					# Quit code
 					print("Renderer received quit. Stopping.")
@@ -122,8 +114,10 @@ func _process(delta: float) -> void:
 						remove_child(currently_loaded_object)
 						currently_loaded_object = node
 					else:
-						print(error_string(node))
-						print('huh')
+						pass
+						# print(error_string(node))
+						# print('huh')
+					
 
 
 			#print('End of match statement')
